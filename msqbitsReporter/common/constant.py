8f5265bf6b53b89dbed5a975f98157bdfe9f45c4
mmohandi <maxime.mohandi@artelys.com>
2019-09-25 13:29:03 +0200

    fix issues : command could not be loaded

--- msqbitsReporter/common/constant.py
+++ msqbitsReporter/common/constant.py
@@ -3,4 +3,4 @@ import os.path as path
 DATABASE_CREDENTIALS_FILE = path.join(path.dirname(path.dirname(path.abspath(__file__))), 'ressources/databaseParameter.json')
 DISCORD_CREDENTIALS_FILE = path.join(path.dirname(path.dirname(path.abspath(__file__))), 'ressources/discordsCredentials.json')
 DISCORD_LOG_FILE = path.join(path.dirname(path.dirname(path.abspath(__file__))), 'ressources/discord_api.log')
-DISCORD_COMMANDS_FILES = ['discordAPI.commands_reporter', 'discordAPI.commands_epsi']
+DISCORD_COMMANDS_FILES = ['msqbitsReporter.discordAPI.commands_reporter', 'msqbitsReporter.discordAPI.commands_epsi']
