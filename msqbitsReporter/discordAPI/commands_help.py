7285a8521d2d2c76a3bed6096fd14da984335f24
Maxime Mohandi <mohandi.maxime@gmail.com>
2019-11-03 13:55:34 +0100

    help commands: still not working

--- msqbitsReporter/discordAPI/commands_help.py
+++ msqbitsReporter/discordAPI/commands_help.py
@@ -1,7 +1,6 @@
-"""
-the base code for this custom help command has been developped here. I don't take any credit for it.
-https://gist.github.com/StudioMFTechnologies/ad41bfd32b2379ccffe90b0e34128b8b
-"""
+"""This custom help command is a perfect replacement for the default one on any Discord Bot written in Discord.py Rewrite!
+However, you must put "bot.remove_command('help')" in your bot, and the command must be in a cog for it to work.
+Written by Jared Newsom (AKA Jared M.F.)!"""
 
 import msqbitsReporter.discordAPI.connector as discordReporter
 from discord.ext import commands
@@ -16,42 +15,50 @@ class HelpCommands(commands.Cog) :
         self.bot = bot
 
     @commands.command(name='help')
-    async def help(self,ctx,*cog):
+    async def help(self, ctx, *cogs):
         """Gets all cogs and commands of mine."""
         try:
-            if not cog:
-                halp=embeds.Embed(title='Cog Listing and Uncatergorized Commands',
-                                  description='Use `$help *cog*` to find out more about them!\n(BTW, the Cog Name Must Be in Title Case, Just Like this Sentence.)',
-                                  color=embededcoulour)
+            if not cogs:
+                self.help_message = embeds.Embed(title='Command List',
+                                                 description='Use `$help *command*` to find out more about them!\n(BTW, The Command Name Must Be in Title Case, Just Like this Sentence.)')
                 cogs_desc = ''
                 for x in self.bot.cogs:
-                    cogs_desc += ('{0} - {1}'.format(x,self.bot.cogs[x].__doc__)+'\n')
-                halp.add_field(name='Cogs',value=cogs_desc[0:len(cogs_desc)-1],inline=False)
+                    cogs_desc += ('{} - {}'.format(x, self.bot.cogs[x].__doc__)+'\n')
+                self.help_message.add_field(name='Cogs',
+                                            value=cogs_desc[0:len(cogs_desc)-1],
+                                            inline=False)
                 cmds_desc = ''
                 for y in self.bot.walk_commands():
-                    if not y.cog_name and not y.hidden:
-                        cmds_desc += ('{0} - {1}'.format(y.name,y.help)+'\n')
-                halp.add_field(name='Uncatergorized Commands',value=cmds_desc[0:len(cmds_desc)-1],inline=False)
-                await ctx.send(embed=halp)
+                    cmds_desc += ('{} - {}'.format(y.name, y.help)+'\n')
+                self.help_message.add_field(name='Uncatergorized Commands',
+                                            value=(cmds_desc[0:len(cmds_desc)-1] if cmds_desc[0:len(cmds_desc)-1] else 'None'),
+                                            inline=False)
+                await ctx.message.add_reaction(emoji='✉')
+                await ctx.message.author.send(embed=self.help_message)
             else:
-                if len(cog) > 1:
-                    halp = embeds.Embed(title='Error!',description='That is way too many cogs!', color= colour.Color.red())
-                    await ctx.send(embed=halp)
+                if len(cogs) > 1:
+                    print('This is way too many cog')
                 else:
                     found = False
                     for x in self.bot.cogs:
-                        for y in cog:
+                        for y in cogs:
                             if x == y:
-                                halp=embeds.Embed(title=cog[0]+' Command Listing',description=self.bot.cogs[cog[0]].__doc__)
+                                self.help_message = embeds.Embed(title=cogs[0]+' Command Listing',
+                                                                 description=self.bot.cogs[cogs[0]].__doc__)
                                 for c in self.bot.get_cog(y).get_commands():
                                     if not c.hidden:
-                                        halp.add_field(name=c.name,value=c.help,inline=False)
+                                        self.help_message.add_field(name=c.name,
+                                                                    value=(c.help if c.help else 'None'),
+                                                                    inline=False)
                                 found = True
                     if not found:
-                        halp = embeds.Embed(title='Error!',description='How do you even use "'+cog[0]+'"?',color=colour.Color.red())
-                    await ctx.send(embed=halp)
-        except Exception as ex:
-            print(ex)
+                        print(f'How do you even use {cogs[0]}?')
+                    else:
+                        await ctx.message.add_reaction(emoji='✉')
+                    print(self.help_message)
+                    await ctx.message.author.send(embed=self.help_message)
+        except Exception as error:
+            print(f'help command error: {error}')
 
 
 def setup(bot):
