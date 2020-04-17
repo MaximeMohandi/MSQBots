60c87fe57445dc0c99c333e95798a19eaefdbadf
Maxime Mohandi <mohandi.maxime@gmail.com>
2020-04-17 10:28:01 +0200

    <fix> deployReporter script not founding built images

--- script/deploy/deployReporter.sh
+++ script/deploy/deployReporter.sh
@@ -2,6 +2,6 @@
 python3.7 config_file_writer.py
 echo "build image"
 cd ../..
-docker build -t msqbitreporter -f script/deploy/Dockerfile .
+docker build -t msbot -f script/deploy/Dockerfile .
 docker run --name MSQbot -d msqbot
 echo "done."
