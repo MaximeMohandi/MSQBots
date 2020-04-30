f8c03d3a1cf6435b322e40d5ce4a241ef2c58e01
Maxime Mohandi <mohandi.maxime@gmail.com>
2020-04-30 16:35:01 +0200

    <fix> deployer script

--- script/deploy/deployReporter.bat
+++ script/deploy/deployReporter.bat
@@ -1,8 +1,15 @@
 @Echo off
 python config_file_writer.py
+
 Echo "build image"
+
 cd ../..
+
 docker build -t msqbot -f %~dp0../deploy/Dockerfile .
 
-docker run -d -it --name MSQbot --volume $pwd/msqbot_log:/app/msqbot/logs:ro msqbot
-Echo done.
+ECHO "run container as service"
+
+docker run -d --name MSQbot -v ./msqbot_logs:/app/logs msqbot
+
+ECHO "Done."
+
--- script/deploy/deployReporter.sh
+++ script/deploy/deployReporter.sh
@@ -3,5 +3,9 @@ python3.7 config_file_writer.py
 echo "build image"
 cd ../..
 docker build -t msqbot -f script/deploy/Dockerfile .
-docker run -d -it --name MSQbot --volume "$(pwd)"/msqbot_log:/app/msqbot/logs:ro msqbot
+
+echo "run container as service"
+
+docker run -d --name MSQbot -v ./msqbot_logs:/app/logs:ro msqbot
+
 echo "done."
