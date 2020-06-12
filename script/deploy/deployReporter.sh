a5c03675d5eb6fa000d30a573d3a4d5bdc6271a8
Maxime Mohandi <mohandi.maxime@gmail.com>
2020-06-12 15:33:22 +0200

    <update> deploy script linux now backup database from meter module

--- script/deploy/deployReporter.sh
+++ script/deploy/deployReporter.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 python3.7 config_file_writer.py
+
+echo "backup last version"
+docker cp MSQbot:/app/msqbot/meter/nice_meter.db .
+
 echo "build image"
 cd ../..
+
+docker stop MSQbot
 docker build -t msqbot -f script/deploy/Dockerfile .
 
 echo "run container as service"
